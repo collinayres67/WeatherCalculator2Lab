80765331258ee26986b496a68ed665ace99fdf16
collinayres67 <59981835+collinayres67@users.noreply.github.com>
2020-01-30 16:20:29 -0600

    Update Program.cs

--- WeatherCalculator2/WeatherCalculator2/Program.cs
+++ WeatherCalculator2/WeatherCalculator2/Program.cs
@@ -12,6 +12,10 @@ namespace WeatherCalculator2
             int windSpeed;
             double windChill;
             string input = "";
+            int RH;
+            double HI;
+
+
 
             // Get the temperature from the user
             Console.WriteLine("Enter the temperature");
@@ -26,6 +30,32 @@ namespace WeatherCalculator2
             input = Console.ReadLine();
 
             windSpeed = int.Parse(input);
+
+            if (temperature > 50)
+            {
+                Console.WriteLine("Enter Relative Humidity");
+                input = Console.ReadLine();
+                RH = int.Parse(input);
+                HI = 0.5 * (temperature + 61 + ((temperature - 68) * 1.2) + (RH * 0.094));
+                Console.WriteLine("The Heat Index is" + HI);
+            }
+            
+           
+           else
+            {
+                windChill = 35.74 + (0.6125 * temperature) - 35.75 * Math.Pow(windSpeed, 0.16) + 0.4275 * temperature * Math.Pow(windSpeed, 0.16);
+                Console.WriteLine("The wind chill is " + windChill);
+            }
+
+
+
+           
+
+
+
+
+
+
 
             // Calculate the wind chill
 
